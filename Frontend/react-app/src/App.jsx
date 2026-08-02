bc90e9b9b0a68a068b0df21b768b082254aa82a2
Noem <noemahmedkhan8307@gmail.com>
2026-08-02 05:05:36 +0500

    Signup Route Updated in About.jsx || Routes Added For Signup and Login in App.jsx

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -1,10 +1,7 @@
-import "./styles/theme.css";       // load once, before component CSS
+import "./styles/theme.css";
 import Home from "./pages/Home/Home";
-import About from "./components/About/About";
-import Features from "./components/Features/Features";
-import Footer from "./components/Footer/Footer";
-// import Signup from "./pages/Signup/Signup";
-// import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -12,8 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/*<Route path="/signup" element={<Signup />} />*/}
-          {/*<Route path="/login" element={<Login />} />*/}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
   )
