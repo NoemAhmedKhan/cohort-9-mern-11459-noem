76212185c73ae62a9c7c4f487a2bf615122cef91
Noem <noemahmedkhan8307@gmail.com>
2026-08-21 17:39:45 +0500

    controllers added || Signup and Login handlers.

---
+++ Backend/node-app/controllers/users.js
@@ -0,0 +1,47 @@
+const User = require("../models/users");
+const { generateJWT } = require("../security/jwt");
+const bcrypt = require("bcrypt");
+
+const handleSignup = async (req, res) => {
+    try{
+        const { fullName, email, password } = req.body;
+        const user = await User.findOne({email: email});
+        if(user) return res.status(400).json({message: `User already exist with this email: ${email}`});
+
+        await User.create(
+            {
+                fullName,
+                email,
+                password
+            });
+
+        return res.status(201).json({message: "Account Created!"});
+    }catch (error) {
+        res.status(400).json({message: "Signup Failed!"});
+    }
+};
+
+const handleLogin = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({email}).select("+password");
+        if(!user) return res.status(400).json({message: "User does not exist!"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message: "Invalid Credentials!"});
+
+        const jwtToken = await generateJWT(user);
+            res.cookie('token', jwtToken, {
+                maxAge: 43200000, // 12 HOUR EXPIRATION TIME
+                httpOnly: true,
+                secure: false,
+                sameSite: 'lax'
+            });
+
+        return res.status(200).json({message: "Login Successful!"});
+    }catch (error) {
+        res.status(401).json({message: "Login Failed!"});
+    }
+}
+
+module.exports = { handleSignup, handleLogin }
