50104776e3b023cc23cdbe26f536674186b5e1e0
Noem <noemahmedkhan8307@gmail.com>
2026-08-22 21:50:16 +0500

    User Routers added for Dashboard and Profile

--- Backend/node-app/routes/users.js
+++ Backend/node-app/routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
-const { validateForm } = require("../middlewares/auth")
-const { handleSignup, handleLogin } = require("../controllers/users");
+const { validateForm, authenticateUser } = require("../middlewares/auth")
+const { handleSignup, handleLogin, handleDashboard, handleProfile, handleEditProfile, handleChangePassword } = require("../controllers/users");
 
 userRouter.post("/signup", validateForm, handleSignup)
-    .post("/login", validateForm, handleLogin);
+    .post("/login", validateForm, handleLogin)
+    .get("/dashboard", authenticateUser, handleDashboard)
+    .get("/profile", authenticateUser, handleProfile)
+    .patch("/profile/edit", authenticateUser, handleEditProfile)
+    .patch("/profile/changepassword", authenticateUser, handleChangePassword);
 
 module.exports = userRouter;
