3d6c6be0c6e300168c70992e8e5b24e0bb7922ea
Noem <noemahmedkhan8307@gmail.com>
2026-08-22 21:13:11 +0500

    Handler functions added for notes crud operations.

---
+++ Backend/node-app/controllers/notes.js
@@ -0,0 +1,53 @@
+const Note = require("../models/notes");
+
+const handleCreateNote = async (req, res) => {
+    try{
+        // INSERT DATA INTO MONGO DB
+        const note = await Note.create(
+            {
+                title: req.title.trim(),
+                content: req.content,
+                user: req.user.id
+            }
+        );
+
+        return res.status(201).json({
+            title: req.title,
+            message: "Note created!"
+        });
+    }catch (error){
+        return res.status(400).json({message: "Error occurred!"});
+    }
+}
+
+const handleViewNote = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const note = await Note.findById(id);
+        return await res.status(200).json(note);
+    }catch (error){
+        return res.status(400).json({message: "Error occurred!"});
+    }
+}
+
+const handleUpdateNote = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Note.findOneAndUpdate({_id: id}, {title: req.title, content: req.content});
+        return await res.status(200).json({id: id, title: req.title, message: "Note updated!"});
+    }catch (error){
+        return res.status(400).json({message: "Error occurred!"});
+    }
+}
+
+const handleDeleteNote = async (req, res) => {
+    try{
+        const id = req.params.id;
+        await Note.deleteOne({_id: id});
+        return await res.status(200).json({id: id, message: "Note deleted!"});
+    }catch (error){
+        return res.status(400).json({message: "Error occurred!"});
+    }
+}
+
+module.exports = { handleCreateNote, handleViewNote, handleUpdateNote, handleDeleteNote };
