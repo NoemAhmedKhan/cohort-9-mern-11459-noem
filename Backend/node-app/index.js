f05e5af9dbdb36049a8959fbf378fb775c8367fb
Noem <noemahmedkhan8307@gmail.com>
2026-08-26 02:54:36 +0500

    CORS security layer added.

--- Backend/node-app/index.js
+++ Backend/node-app/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
+const CORS = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./connection");
 const userRouter = require("./routes/users");
 const notesRouter = require("./routes/notes");
 const { PORT } = require("./config/env");
 
+app.use(CORS({
+        origin: 'http://localhost:5173',
+        credentials: true
+    })
+)
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json({extended: true}));
-app.use(cookieParser());
 app.use("/", userRouter);
 app.use("/notes", notesRouter);
 
